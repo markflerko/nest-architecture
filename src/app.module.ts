61c4fb69dd8ac896af09a93644170fad44b0da47
markflerko <markflerko@gmail.com>
2025-05-27 14:09:42 +0300

    16. event store part-1 init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ApplicationBootstrapOptions } from 'src/common/interfaces/application-b
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
